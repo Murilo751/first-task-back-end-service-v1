e53f0c5573c0063772826adf5842d21b1920ea87
murilo.aragao <murilo.matos2005@gmail.com>
2024-09-11 08:42:56 -0300

    Initial commit

---
+++ src/main/java/com/example/firsttask/FirstTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.firsttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firsttask/FirstTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firsttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
