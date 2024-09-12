12744026604814c524f9e73c5f2300336506be96
Murilo751 <murilo.matos2005@gmail.com>
2024-09-12 16:02:34 -0300

    refactor: Repository

--- src/main/java/com/example/firsttask/repository/UserRepository.java
+++ src/main/java/com/example/firsttask/repository/UserRepository.java
@@ -2,5 +2,7 @@ package com.example.firsttask.repository;
 
 import com.example.firsttask.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> { }
