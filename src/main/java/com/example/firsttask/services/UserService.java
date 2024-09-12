c8965314ed0b072f8a1ef4f9a3dc4c402133d837
murilo.aragao <murilo.matos2005@gmail.com>
2024-09-12 11:13:43 -0300

    refactor: Service

--- src/main/java/com/example/firsttask/services/UserService.java
+++ src/main/java/com/example/firsttask/services/UserService.java
@@ -3,18 +3,19 @@ package com.example.firsttask.services;
 import com.example.firsttask.entity.User;
 import com.example.firsttask.repository.UserRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
 @Transactional
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public User registerUser(User user) {
         return userRepository.save(user);
